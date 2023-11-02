3f6384d18a9a114fe3088323e10b6be383de9a25
Maverick Grayer <maverick72032@gmail.com>
2023-11-02 15:57:42 -0400

    Varibles exercise complete

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "Maverick";
+            int myPowerLevel = 9001;
+            char myRank = 'S';
+            bool isAwesome = true;
+            double percentPowerDisplayed = 0.1;
+            decimal futureBankAccount = 999999.99m;
+
+            Console.WriteLine($"My name is {myName}.");
+            Console.WriteLine("My power level is " + myPowerLevel + ".");
+            Console.WriteLine("My current Hunter rank " + myRank + ".");
+            Console.WriteLine("The poll's asking if I am awesome came back, and everyone answered " + isAwesome + ".");
+            Console.WriteLine("I have only shown " + percentPowerDisplayed + "% of my power!");
+            Console.WriteLine("Soon my bank account will show " + futureBankAccount + " dollars.");
         }
     }
 }
